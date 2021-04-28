b12a3ac5b33774f5d262aa22ecf2aa935512ae23
José Ramírez Benítez <jose310102@gmail.com>
2021-04-28 16:40:30 +0200

    commit con javadoc

--- src/main/java/clases/Ordenador.java
+++ src/main/java/clases/Ordenador.java
@@ -1,7 +1,18 @@
 package clases;
 import java.util.Random;
+/**
+ * Esta clase simula el comportamiento de un semáforo. El color devuelto es
+ * aleatorio.
+ * @author Jose
+ * @version 2.0
+ * @since 1.5
+ * @see <a href = https://docs.oracle.com/javase/7/docs/api/java/util/Random.html> Clase
+ */
 public class Ordenador {
-    public Ordenador() {}
+    /**
+     * Devuelve un valor aleatorio entre rojo, amarillo y verde
+     * @return Color del semaforo que responde el ordenador
+     */
     public String color(){
     	Random randomGenerator = new Random();
 	int randomInt = randomGenerator.nextInt(3);
